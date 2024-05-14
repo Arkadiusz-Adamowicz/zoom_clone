5e35ae0de482be860e1b6419fac7b5704897c8b2
Arkadiusz Adamowicz <arek.adamowicz81@gmail.com>
2024-05-14 02:48:42 +0200

    first commit

---
+++ src/app/(root)/meeting/[id]/page.tsx
@@ -0,0 +1,35 @@
+'use client';
+
+import { useUser } from '@clerk/nextjs';
+import { useState } from 'react';
+import { StreamTheme, StreamCall } from '@stream-io/video-react-sdk';
+import MeetingSetup from '@/components/MeetingSetup';
+import MeetingRoom from '@/components/MeetingRoom';
+import { useGetCallById } from '../../../../../hooks/useGetCallById';
+import { Loader } from 'lucide-react';
+import { useParams } from 'next/navigation';
+
+const Meeting = () => {
+  const { id } = useParams();
+  const { user, isLoaded } = useUser();
+  const [isSetupComplete, setIsSetupComplete] = useState(false);
+  const { call, isCallLoading } = useGetCallById(id);
+
+  if (!isLoaded || isCallLoading) return <Loader />;
+
+  return (
+    <main className='h-screen w-full'>
+      <StreamCall call={call}>
+        <StreamTheme>
+          {!isSetupComplete ? (
+            <MeetingSetup setIsSetupComplete={setIsSetupComplete} />
+          ) : (
+            <MeetingRoom />
+          )}
+        </StreamTheme>
+      </StreamCall>
+    </main>
+  );
+};
+
+export default Meeting;
